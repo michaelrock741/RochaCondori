4b04bb9fe581c10efdcef407166413ab48261b62
unknown <169000171+michaelrock741@users.noreply.github.com>
2024-06-06 15:14:09 -0400

    Subida de Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { SliderComponent } from './slider/slider.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule,CabeceraComponent,SliderComponent, BodyComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
-})
+}
+)
 export class AppComponent {
   title = 'RochaCondori';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { SliderComponent } from './slider/slider.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'cabecera', component:CabeceraComponent
+    },
+    {
+        path:'slider', component:SliderComponent
+    },
+    {
+        path:'body', component:BodyComponent
+    },
+    {
+        path:'footer', component:FooterComponent
+    }
+
+];
